5c45d78bc78dfb66c30268e080eaa30312925c65
Beirens Bram <be08049@beirenss-mbp.aag-de.ibmmobiledemo.com>
2018-09-24 15:45:27 +0200

    empty support added

--- smartsenseexpress/app.js
+++ smartsenseexpress/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var coolRouter = require('./routes/cool');
 var dashboardRouter = require('./routes/dashboard');
+var supportRouter = require('./routes/dashboard');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/users/cool', coolRouter);
 app.use('/dashboard', dashboardRouter);
+app.use('/support', supportRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
