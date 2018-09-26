786eaf4cfa9f1e1277129d189e89a9dfe973e126
bram.beirens@gmail.com <bn326160>
2018-09-26 09:56:31 +0200

    update

--- smartsenseexpress/routes/lead.js
+++ smartsenseexpress/routes/lead.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('status', {
-        title: 'Status',
+    res.render('lead', {
+        title: 'Lead Dashboard',
     });
 });
 
